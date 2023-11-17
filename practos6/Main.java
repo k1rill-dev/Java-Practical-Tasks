31f89de6754a18c6c65ebeeec56322e82a4ea2d8
Кирилл Белокобыльский <63358980+k1rill-dev@users.noreply.github.com>
2023-11-17 17:00:16 +0300

    Update Main.java

--- practos6/Main.java
+++ practos6/Main.java
@@ -1,5 +1,4 @@
 package practos6;
-
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
@@ -7,31 +6,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int width = 5; int height = 5;
-        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+        int[][] matrix = generateMatrix(width, height);
+        showMatrix(matrix, width, height);
+        try{
+            System.out.println("Enter column:");
+            int col = scanner.nextInt();
+            showColumnMatrix(matrix, col);
+        }catch (ArrayIndexOutOfBoundsException arrayException){
+            System.out.println("There is no column with such index!");
+        }catch (InputMismatchException inputException){
+            System.out.println("Value must be integer!");
+        }
+    }
+    public static int[][] generateMatrix(int width, int height){
+        Random random = new Random();
         int[][] matrix = new int[width][height];
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 matrix[i][j] = random.nextInt(1, 20);
             }
         }
+        return matrix;
+    }
+    public static void showMatrix(int[][] matrix, int width, int height){
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 System.out.printf("%d\t", matrix[i][j]);
             }
             System.out.print("\n");
         }
-        try{
-            System.out.println("Enter column:");
-            int col = scanner.nextInt();
-            for (int i = 0; i < width; i++) {
-                System.out.println(matrix[i][col]);
-            }
-        }catch (ArrayIndexOutOfBoundsException arrayException){
-            System.out.println("There is no column with such index!");
-        } catch (InputMismatchException inputException){
-            System.out.println("Value must be integer!");
+    }
+    public static void showColumnMatrix(int[][] matrix, int column){
+        for (int[] ints : matrix) {
+            System.out.println(ints[column]);
         }
-
     }
 }
